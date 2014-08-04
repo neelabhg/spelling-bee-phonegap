0b87238677d5eaa1e1f869712a86a45345fc9dba
Neelabh Gupta <ngupta@twitter.com>
2014-08-04 09:21:17 -0700

    Implemented spelling UI, other things. Main game functionality is done

--- www/js/app.js
+++ www/js/app.js
@@ -1,36 +1,164 @@
 // The main application
-var app = (function($, datasource) {
+var app = (function ($, datasource) {
 
-    var startGame;
+    var maxWords, maxTries, ds;
 
-    startGame = function (difficulty, numWords) {
-	var ds;
+	var init = function () {
 
-	console.log("Starting game.");
-	ds = datasource("wordnik");
-	ds.getRandomWords(numWords, function (data) {
-	    console.log(data);
-	});
+    	maxWords = 5;
+    	maxTries = 5;
+
+    	ds = datasource("wordnik");
+
+	    $("#startButton").on("click", function () {
+		    loadNextWord(1, {});
+	    });
+
+	    $("#homeButton").on("click", function () {
+	    	changePage("#homeNavigationConfirmationDialogPage");
+	    });
+
+	    $("#exitButton").on("click", function () {
+	    	navigator.app.exitApp();
+	    });
     };
 
     $(document).on("ready", function () {
-
-	var numWords = 10;
-	
-        $("#startButtonEasy").on("click", function () {
-	    startGame(0, numWords);
-        });
-	$("#startButtonMedium").on("click", function () {
-	    startGame(1, numWords);
-        });
-	$("#startButtonHard").on("click", function () {
-	    startGame(2, numWords);
-        });
-        
+    	init();
     });
 
-    $(document).on("deviceready", function() {
+    $(document).on("deviceready", function () {
     });
+
+    $(document).on("backbutton", function (e) {
+    	// From http://stackoverflow.com/a/8662401
+    	if ($.mobile.activePage.is('#mainPage')) {
+			e.preventDefault();
+			navigator.app.exitApp();
+	    } else {
+			navigator.app.backHistory()
+	    }
+    });
+
+    var changePage = function (target) {
+    	$(":mobile-pagecontainer").pagecontainer("change", target, {});
+    };
+
+    var showLoadingSpinner = function (text) {
+    	$.mobile.loading('show', {
+			text: text,
+			textVisible: true,
+			theme: 'z',
+			html: ""
+		});
+    };
+
+    var hideLoadingSpinner = function () {
+    	$.mobile.loading('hide');
+    };
+
+    var loadNextWord = function (wordNum, accumulatedResults) {
+		changePage("#emptyPage");
+		showLoadingSpinner("Loading next word");
+		ds.getRandomWords(1, function (newWordData) {
+		    hideLoadingSpinner();
+		    spellView(wordNum, newWordData[0], accumulatedResults);
+		});
+    };
+
+    var spellView = function (wordNum, word, results) {
+    	
+		var numTries = 0;
+		var wordAudio;
+
+		if (window.Media) {
+			wordAudio = new Media(word.audioUrl);
+		} else {
+			// Media not supported
+			wordAudio = {
+				// for debugging
+				play: function () { console.log(word); }
+			};
+		}
+
+		// Attach click handlers to the buttons in the UI.
+		// First remove the already attached event handlers because we set these
+		// handlers every time spellView is called, i.e, when loading a new word.
+		// If we don't remove the previous handlers, the new handlers will get
+		// attached in addition to the already attached handlers, and when the
+		// event fires, all the previously attached handlers will also be called,
+		// which will wreak havoc.
+
+		$("#spellingPage").off("pagebeforeshow").on("pagebeforeshow", function () {
+			$("#numWordsDisplay").text(wordNum);
+			$("#maxWordsDisplay").text(maxWords);
+			$("#wordProgressBar").val((wordNum / maxWords) * 100);
+			$("#numTriesDisplay").text(numTries);
+			$("#maxTriesDisplay").text(maxTries);
+			$("#wordDefinitionDisplay").html(word.definitions[0]);
+			$("#wordInput").val("");
+			$("#nextWordButton").text((wordNum === maxWords) ? "Show results" : "Next word");
+
+			// From http://stackoverflow.com/a/23728076
+		    $("#wordProgressBar").hide();
+		    $(".ui-slider-handle").hide();
+		    $('.ui-slider-track').css('margin','0 15px 0 15px').css('pointer-events','none');
+
+		    $("#wordProgressBar").slider("refresh");
+		    $("#wordDefinitionCollapsible").collapsible("collapse");
+		});
+
+		$("#nextWordButton").off("click").on("click", function () {
+			if (wordNum === maxWords) {
+				displayResults(results);
+			} else {
+				loadNextWord(wordNum + 1, results);
+			}
+		});
+
+		$("#sayWordButton").off("click").on("click", function () {
+			wordAudio.play();
+		});
+
+		$("#showAnswerButton").off("click").on("click", function () {
+			results['skipped'] = (results['skipped'] || 0) + 1;
+			
+			$("#dialogHeading").text("Skipped word");
+			$("#correctWord").text(word.word);
+	    	changePage("#wordDoneDialogPage");
+	    });
+
+	    $("#checkAnswerButton").off("click").on("click", function () {
+	    	numTries += 1;
+	    	if ($("#wordInput").val().toLowerCase() === word.word.toLowerCase()) {
+	    		// correct answer
+	    		results['correct'] = (results['correct'] || 0) + 1;
+	    		$("#dialogHeading").text("Correct answer!");
+	    		$("#correctWord").text(word.word);
+	    		changePage("#wordDoneDialogPage");
+	    	} else {
+	    		// incorrect answer
+	    		if (numTries === maxTries) {
+	    			// max number of tries reached
+	    			results['incorrect'] = (results['incorrect'] || 0) + 1;
+	    			$("#dialogHeading").text("Incorrect answer");
+	    			$("#correctWord").text(word.word);
+	    			changePage("#wordDoneDialogPage");
+	    		} else {
+	    			$("#numTriesDisplay").text(numTries);
+	    		}
+	    	}
+	    });
+
+	    changePage("#spellingPage");
+    };
+
+    var displayResults = function (results) {
+    	$("#correctWordsCount").text(results['correct'] || 0);
+    	$("#incorrectWordsCount").text(results['incorrect'] || 0);
+    	$("#skippedWordsCount").text(results['skipped'] || 0);
+    	changePage("#resultsPage");
+    };
 
     return {};
 
--- www/js/datasources/wordnik/wordnik_client.js
+++ www/js/datasources/wordnik/wordnik_client.js
@@ -4,58 +4,58 @@ var wordnik_client = (function ($, api_key) {
     var base_url = "http://api.wordnik.com:80/v4/";
 
     var getRandomWord = function (successFunc) {
-	$.getJSON(
-	    base_url + "words.json/randomWord",
-	    {
-		api_key: api_key
-	    },
-	    function (word) {
-		successFunc(word.word);
-	    }
-	);
+		$.getJSON(
+		    base_url + "words.json/randomWord",
+		    {
+				api_key: api_key
+		    },
+		    function (word) {
+				successFunc(word.word);
+		    }
+		);
     };
 
     var getWordDefinitions = function (word, successFunc) {
-	$.getJSON(
-	    base_url + "word.json/" + word + "/definitions",
-	    {
-		api_key: api_key
-	    },
-	    function (definitions) {
-		// filter the returned JSON to contain only the
-		// word definitions, and then call successFunc
-		var i, filtered = [];
-		for (i = 0; i < definitions.length; i++) {
-		    filtered[i] = definitions[i].text;
-		}
-		successFunc(filtered);
-	    }
-	);
+		$.getJSON(
+		    base_url + "word.json/" + word + "/definitions",
+		    {
+				api_key: api_key
+		    },
+		    function (definitions) {
+				// filter the returned JSON to contain only the
+				// word definitions, and then call successFunc
+				var i, filtered = [];
+				for (i = 0; i < definitions.length; i++) {
+				    filtered[i] = definitions[i].text;
+				}
+				successFunc(filtered);
+		    }
+		);
     };
 
     var getWordAudioUrl = function (word, successFunc) {
-	$.getJSON(
-	    base_url + "word.json/" + word + "/audio",
-	    {
-		api_key: api_key,
-		limit: 1
-	    },
-	    function (audioFiles) {
-		// filter the returned JSON to contain only the
-		// fileUrls, and then call successFunc with one result
-		var i, filtered = [];
-		for (i = 0; i < audioFiles.length; i++) {
-		    filtered[i] = audioFiles[i].fileUrl;
-		}
-		successFunc(filtered[0]);
-	    }
-	);
+		$.getJSON(
+		    base_url + "word.json/" + word + "/audio",
+		    {
+				api_key: api_key,
+				limit: 1
+		    },
+		    function (audioFiles) {
+				// filter the returned JSON to contain only the
+				// fileUrls, and then call successFunc with one result
+				var i, filtered = [];
+				for (i = 0; i < audioFiles.length; i++) {
+				    filtered[i] = audioFiles[i].fileUrl;
+				}
+				successFunc(filtered[0]);
+		    }
+		);
     };
 
     return {
-	getRandomWord: getRandomWord,
-	getWordDefinitions: getWordDefinitions,
-	getWordAudioUrl: getWordAudioUrl
+		getRandomWord: getRandomWord,
+		getWordDefinitions: getWordDefinitions,
+		getWordAudioUrl: getWordAudioUrl
     };
 
 })(jQuery, wordnik_api_key);
