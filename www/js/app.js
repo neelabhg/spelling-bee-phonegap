3fe9584b460470a582dc2624c0cab4697e85f913
Neelabh Gupta <ngupta@twitter.com>
2014-08-08 01:31:58 -0700

    Added settings, word progress bar, improved design, etc

--- www/js/app.js
+++ www/js/app.js
@@ -1,163 +1,228 @@
 // The main application
 var app = (function ($, datasource) {
 
-    var maxWords, maxTries, ds;
+    var ds, settings;
 
-	var init = function () {
+    var defaultSettings = {
+        maxWords: 5,
+        maxTries: 5
+    };
 
-    	maxWords = 5;
-    	maxTries = 5;
+    var settings;
 
-    	ds = datasource("wordnik");
+    var init = function () {
 
-	    $("#startButton").on("click", function () {
-		    loadNextWord(1, {});
-	    });
+        settings = loadSettings() || defaultSettings;
+        
+        ds = datasource("wordnik");
 
-	    $("#homeButton").on("click", function () {
-	    	changePage("#homeNavigationConfirmationDialogPage");
-	    });
+        $("#startButton").on("click", function () {
+            loadNextWord(1, {});
+        });
 
-	    $("#exitButton").on("click", function () {
-	    	navigator.app.exitApp();
-	    });
+        $("#exitButton").on("click", function () {
+            navigator.app.exitApp();
+        });
+
+        $("#settingsPageBackButton").on("click", handleDiscardSettingsAction);
+        $("#discardSettingsButton").on("click", handleDiscardSettingsAction);
+
+        $("#restoreDefaultsSettingsButton").on("click", function (e) {
+            if (JSON.stringify(getUserSettings()) !== JSON.stringify(defaultSettings)) {
+                // entered settings are different than default settings
+                $("#restoreDefaultSettingsConfirmationPopup").popup("open");
+                e.preventDefault();
+            }
+        });
+
+        $("#confirmDiscardSettingsButton").on("click", function () {
+            setUserSettings(settings);
+        });
+
+        $("#confirmRestoreDefaultSettingsButton").on("click", function () {
+            setUserSettings(defaultSettings);
+        });
+
+        $("#saveSettingsButton").on("click", function (e) {
+            saveSettings(getUserSettings());
+        });
+
+        $("#settingsPage").on("pagebeforeshow", function () {
+            setUserSettings(settings);
+        });
+    };
+
+    var handleDiscardSettingsAction = function (userClickEvent) {
+        if (JSON.stringify(settings) !== JSON.stringify(getUserSettings())) {
+            // unsaved settings
+            $("#discardSettingsChangesConfirmationPopup").popup("open");
+            userClickEvent.preventDefault();
+        } else {
+            changePage("#mainPage");
+        }
+    };
+
+    var saveSettings = function (settings) {
+        localStorage.setItem("settings", JSON.stringify(settings));
+    };
+
+    var loadSettings = function () {
+        // From http://stackoverflow.com/a/3146971
+        var settings = localStorage.getItem("settings");
+        if (settings) {
+            settings = JSON.parse(settings);
+            settings.maxWords = parseInt(settings.maxWords);
+            settings.maxTries = parseInt(settings.maxTries);
+        }
+        return settings;
+    };
+
+    var getUserSettings = function () {
+        return {
+            maxWords: parseInt($("#numWordsSettingsSlider").val()),
+            maxTries: 5
+        };
+    };
+
+    var setUserSettings = function (settings) {
+        $("#numWordsSettingsSlider").val(settings.maxWords);
+        $("#numWordsSettingsSlider").slider("refresh");
     };
 
     $(document).on("ready", function () {
-    	init();
+        init();
     });
 
     $(document).on("deviceready", function () {
     });
 
     $(document).on("backbutton", function (e) {
-    	// From http://stackoverflow.com/a/8662401
-    	if ($.mobile.activePage.is('#mainPage')) {
-			e.preventDefault();
-			navigator.app.exitApp();
-	    } else {
-			navigator.app.backHistory()
-	    }
+        // From http://stackoverflow.com/a/8662401
+        if ($.mobile.activePage.is('#mainPage')) {
+            e.preventDefault();
+            navigator.app.exitApp();
+        } else if ($.mobile.activePage.is('#spellingPage')) {
+            $("#homeNavigationConfirmationPopup").popup("open");
+            e.preventDefault();
+        } else if ($.mobile.activePage.is('#settingsPage')) {
+            handleDiscardSettingsAction(e);
+        } else {
+            navigator.app.backHistory()
+        }
     });
 
     var changePage = function (target) {
-    	$(":mobile-pagecontainer").pagecontainer("change", target, {});
+        $(":mobile-pagecontainer").pagecontainer("change", target, {});
     };
 
     var showLoadingSpinner = function (text) {
-    	$.mobile.loading('show', {
-			text: text,
-			textVisible: true,
-			theme: 'z',
-			html: ""
-		});
+        $.mobile.loading('show', {
+            text: text,
+            textVisible: true,
+            theme: 'z',
+            html: ""
+        });
     };
 
     var hideLoadingSpinner = function () {
-    	$.mobile.loading('hide');
+        $.mobile.loading('hide');
     };
 
     var loadNextWord = function (wordNum, accumulatedResults) {
-		changePage("#emptyPage");
-		showLoadingSpinner("Loading next word");
-		ds.getRandomWords(1, function (newWordData) {
-		    hideLoadingSpinner();
-		    spellView(wordNum, newWordData[0], accumulatedResults);
-		});
+        changePage("#emptyPage");
+        showLoadingSpinner("Loading next word");
+        ds.getRandomWords(1, function (newWordData) {
+            hideLoadingSpinner();
+            spellView(wordNum, newWordData[0], accumulatedResults);
+        });
     };
 
     var spellView = function (wordNum, word, results) {
-    	
-		var numTries = 0;
-		var wordAudio;
+        
+        var numTries = 0;
+        var wordAudio;
 
-		if (window.Media) {
-			wordAudio = new Media(word.audioUrl);
-		} else {
-			// Media not supported
-			wordAudio = {
-				// for debugging
-				play: function () { console.log(word); }
-			};
-		}
+        if (window.Media) {
+            wordAudio = new Media(word.audioUrl);
+        } else {
+            // Media not supported
+            wordAudio = {
+                // for debugging
+                play: function () { console.log(word); }
+            };
+        }
 
-		// Attach click handlers to the buttons in the UI.
-		// First remove the already attached event handlers because we set these
-		// handlers every time spellView is called, i.e, when loading a new word.
-		// If we don't remove the previous handlers, the new handlers will get
-		// attached in addition to the already attached handlers, and when the
-		// event fires, all the previously attached handlers will also be called,
-		// which will wreak havoc.
+        // Attach click handlers to the buttons in the UI.
+        // First remove the already attached event handlers because we set these
+        // handlers every time spellView is called, i.e, when loading a new word.
+        // If we don't remove the previous handlers, the new handlers will get
+        // attached in addition to the already attached handlers, and when the
+        // event fires, all the previously attached handlers will also be called,
+        // which will wreak havoc.
 
-		$("#spellingPage").off("pagebeforeshow").on("pagebeforeshow", function () {
-			$("#numWordsDisplay").text(wordNum);
-			$("#maxWordsDisplay").text(maxWords);
-			$("#wordProgressBar").val((wordNum / maxWords) * 100);
-			$("#numTriesDisplay").text(numTries);
-			$("#maxTriesDisplay").text(maxTries);
-			$("#wordDefinitionDisplay").html(word.definitions[0]);
-			$("#wordInput").val("");
-			$("#nextWordButton").text((wordNum === maxWords) ? "Show results" : "Next word");
+        $("#spellingPage").off("pagebeforeshow").on("pagebeforeshow", function () {
+            $("#numWordsDisplay").text(wordNum);
+            $("#maxWordsDisplay").text(settings.maxWords);
+            $("#wordProgressBar").val((wordNum / settings.maxWords) * 100);
+            $("#numTriesDisplay").text(numTries);
+            $("#maxTriesDisplay").text(settings.maxTries);
+            $("#wordDefinitionDisplay").html(word.definitions[0]);
+            $("#wordInput").val("");
+            $("#nextWordButton").text((wordNum === settings.maxWords) ? "Show results" : "Next word");
+            $("#wordProgressBar").slider("refresh");
+            $("#wordDefinitionCollapsible").collapsible("collapse");
+        });
 
-			// From http://stackoverflow.com/a/23728076
-		    $("#wordProgressBar").hide();
-		    $(".ui-slider-handle").hide();
-		    $('.ui-slider-track').css('margin','0 15px 0 15px').css('pointer-events','none');
+        $("#nextWordButton").off("click").on("click", function () {
+            if (wordNum === settings.maxWords) {
+                displayResults(results);
+            } else {
+                loadNextWord(wordNum + 1, results);
+            }
+        });
 
-		    $("#wordProgressBar").slider("refresh");
-		    $("#wordDefinitionCollapsible").collapsible("collapse");
-		});
+        $("#sayWordButton").off("click").on("click", function () {
+            wordAudio.play();
+        });
 
-		$("#nextWordButton").off("click").on("click", function () {
-			if (wordNum === maxWords) {
-				displayResults(results);
-			} else {
-				loadNextWord(wordNum + 1, results);
-			}
-		});
+        $("#showAnswerButton").off("click").on("click", function () {
+            results['skipped'] = (results['skipped'] || 0) + 1;
+            
+            $("#dialogHeading").text("Skipped word");
+            $("#correctWord").text(word.word);
+            $("#wordDonePopup").popup("open");
+        });
 
-		$("#sayWordButton").off("click").on("click", function () {
-			wordAudio.play();
-		});
+        $("#checkAnswerButton").off("click").on("click", function () {
+            numTries += 1;
+            if ($("#wordInput").val().toLowerCase() === word.word.toLowerCase()) {
+                // correct answer
+                results['correct'] = (results['correct'] || 0) + 1;
+                $("#dialogHeading").text("Correct answer!");
+                $("#correctWord").text(word.word);
+                $("#wordDonePopup").popup("open");
+            } else {
+                // incorrect answer
+                if (numTries === settings.maxTries) {
+                    // max number of tries reached
+                    results['incorrect'] = (results['incorrect'] || 0) + 1;
+                    $("#dialogHeading").text("Incorrect answer");
+                    $("#correctWord").text(word.word);
+                    $("#wordDonePopup").popup("open");
+                } else {
+                    $("#numTriesDisplay").text(numTries);
+                }
+            }
+        });
 
-		$("#showAnswerButton").off("click").on("click", function () {
-			results['skipped'] = (results['skipped'] || 0) + 1;
-			
-			$("#dialogHeading").text("Skipped word");
-			$("#correctWord").text(word.word);
-	    	changePage("#wordDoneDialogPage");
-	    });
-
-	    $("#checkAnswerButton").off("click").on("click", function () {
-	    	numTries += 1;
-	    	if ($("#wordInput").val().toLowerCase() === word.word.toLowerCase()) {
-	    		// correct answer
-	    		results['correct'] = (results['correct'] || 0) + 1;
-	    		$("#dialogHeading").text("Correct answer!");
-	    		$("#correctWord").text(word.word);
-	    		changePage("#wordDoneDialogPage");
-	    	} else {
-	    		// incorrect answer
-	    		if (numTries === maxTries) {
-	    			// max number of tries reached
-	    			results['incorrect'] = (results['incorrect'] || 0) + 1;
-	    			$("#dialogHeading").text("Incorrect answer");
-	    			$("#correctWord").text(word.word);
-	    			changePage("#wordDoneDialogPage");
-	    		} else {
-	    			$("#numTriesDisplay").text(numTries);
-	    		}
-	    	}
-	    });
-
-	    changePage("#spellingPage");
+        changePage("#spellingPage");
     };
 
     var displayResults = function (results) {
-    	$("#correctWordsCount").text(results['correct'] || 0);
-    	$("#incorrectWordsCount").text(results['incorrect'] || 0);
-    	$("#skippedWordsCount").text(results['skipped'] || 0);
-    	changePage("#resultsPage");
+        $("#correctWordsCount").text(results['correct'] || 0);
+        $("#incorrectWordsCount").text(results['incorrect'] || 0);
+        $("#skippedWordsCount").text(results['skipped'] || 0);
+        changePage("#resultsPage");
     };
 
     return {};
